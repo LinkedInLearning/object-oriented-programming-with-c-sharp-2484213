4b02e118df2c7a86f6160c63a103a199607a47ef
Jesse Freeman <jessefreeman@gmail.com>
2022-10-11 11:36:15 -0700

    Adding a player class.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿Console.WriteLine("Hello, what is your name?");
+﻿using OOPAdventure;
+
+Console.WriteLine("Hello, what is your name?");
 
 var name = Console.ReadLine();
 
 if (name == String.Empty)
     name = "No Name";
 
-Console.WriteLine("Welcome {0} to your OOP Adventure!", name);
+var player = new Player(name);
+
+Console.WriteLine("Welcome {0} to your OOP Adventure!", player.Name);
---
+++ Src/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPAdventure;
+
+public class Player : Character
+{
+
+    public Player(string name) : base(name)
+    {
+
+    }
+
+}
