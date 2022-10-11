835b6d4634c820a65b2bba0d9d133a1cac262e1d
Jesse Freeman <jessefreeman@gmail.com>
2022-10-11 15:31:27 -0700

    Added abstract action class.

---
+++ Src/Actions/Action.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPAdventure;
+
+public abstract class Action
+{
+
+    public virtual string Name => "";
+
+    public virtual void Execute(string[] args)
+    {
+        throw new Exception("Nothing to execute");
+    }
+
+}
---
+++ Src/Actions/Actions.cs
@@ -0,0 +1,28 @@
+﻿namespace OOPAdventure;
+
+public sealed class Actions
+{
+
+    private static Actions _instance;
+
+    public static Actions Instance
+    {
+
+        get
+        {
+            if (_instance == null)
+                _instance = new Actions();
+
+            return _instance;
+        }
+
+    }
+
+    private readonly Dictionary<string, Action> _registeredActions = new();
+
+    private Actions()
+    {
+
+    }
+
+}
