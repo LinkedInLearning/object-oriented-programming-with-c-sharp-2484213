a431dc6f4d493b1b2e4485d5e4a1b083db92ce4e
Jesse Freeman <jessefreeman@gmail.com>
2022-10-11 11:25:26 -0700

    Initia import.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+if (name == String.Empty)
+    name = "No Name";
+
+Console.WriteLine("Welcome {0} to your OOP Adventure!", name);
