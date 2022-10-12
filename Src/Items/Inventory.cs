0c272a233b816e917566404ae823f429567422c9
Jesse Freeman <jessefreeman@gmail.com>
2022-10-12 09:44:37 -0700

    Added the inventory.

---
+++ Src/Items/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using OOPAdventure;
+
+public class Inventory
+{
+
+    private List<Item> Items { get; } = new();
+
+    public int Total => Items.Count;
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+}
