567fe48b9475c5803f88a8ca4c4e7717b063c73c
Jesse Freeman <jessefreeman@gmail.com>
2022-10-12 09:34:38 -0700

    Added item.

---
+++ Src/Items/Item.cs
@@ -0,0 +1,19 @@
+﻿using OOPAdventure;
+
+public abstract class Item
+{
+
+    public virtual string Name { get; set; }
+
+    public bool SingleUse { get; set; }
+
+    public bool CanTake { get; set; } = true;
+
+    public virtual void Use(string source)
+    {
+
+        throw new NotImplementedException();
+
+    }
+
+}
